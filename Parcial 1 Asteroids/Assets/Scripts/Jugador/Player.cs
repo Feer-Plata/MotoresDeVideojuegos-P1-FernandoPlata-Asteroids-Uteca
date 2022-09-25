df55086b62f06cb2ad7c3c00b80da8ace6de5a05
Feer-Plata <fernando.platacbc@gmail.com>
2022-09-25 00:22:39 -0500

    PARCIAL1-Momienento de jugador y creacion de proyecto
    Creacion de proyecot en Unity y primer scripts para el movimiento del jugador.

---
+++ Parcial 1 Asteroids/Assets/Scripts/Jugador/Player.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    //Atributos
+    public int velocidad;
+
+    void Update()
+    {
+        MovimientoJugador();
+    }
+    public void MovimientoJugador()
+    {
+        //Movimiento a hacia Arriba.
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(0, velocidad * Time.deltaTime, 0, Space.Self);
+        }
+        //Movimiento a la Izquierda.
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(-velocidad * Time.deltaTime, 0, 0, Space.Self);
+        }
+        //Movimiento a la Derecha.
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(velocidad * Time.deltaTime, 0, 0, Space.Self);
+        }
+        //Movimiento a hacia Arriba.
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(0, -velocidad * Time.deltaTime, 0, Space.Self);
+        }
+    }
+}
