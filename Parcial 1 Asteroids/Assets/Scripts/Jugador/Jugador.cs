b8053311e08db5469e681f9f22e5af9d29ed12d8
Feer-Plata <fernando.platacbc@gmail.com>
2022-10-01 22:49:21 -0500

    Parcial-1 Movimiento jugador

---
+++ Parcial 1 Asteroids/Assets/Scripts/Jugador/Jugador.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jugador : MonoBehaviour
+{
+    //Atributos
+
+    //Movimiento del jugador
+    public static float Vertical 
+    {
+        get { return Input.GetAxis("Vertical"); }
+    }
+    public static float Vertical
+    {
+        get { return Input.GetAxis("Vertical"); }
+    }
+}
