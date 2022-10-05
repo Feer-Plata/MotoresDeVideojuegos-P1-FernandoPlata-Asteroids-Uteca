730645e59d5d5d21c918cb42a1125614d9c45e54
Feer-Plata <fernando.platacbc@gmail.com>
2022-10-05 11:39:30 -0500

    PARCIA1 Muerte de jugador
    Implementacion de vida y escena de muerte

--- Parcial 1 Asteroids/Assets/Scripts/Jugador/Player.cs
+++ Parcial 1 Asteroids/Assets/Scripts/Jugador/Player.cs
@@ -1,37 +1,79 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Player : MonoBehaviour
 {
-    //Atributos
-    public int velocidad;
+    //Atributos de jugador.
+    public int velocidadJugador;
+    public int vidaJugador;
+    public Slider barraVidaJugador;
+
+    //Atributos de disparo de jugador.
+    public GameObject bala;
+    public List<Transform> posicionesBala;
 
     void Update()
     {
         MovimientoJugador();
+        CheckLife();
+        DisparoJugador();
+        BajarvidaJugador();
     }
     public void MovimientoJugador()
     {
         //Movimiento a hacia Arriba.
         if (Input.GetKey(KeyCode.W))
         {
-            transform.Translate(0, velocidad * Time.deltaTime, 0, Space.Self);
+            transform.Translate(0, velocidadJugador * Time.deltaTime, 0, Space.Self);
         }
         //Movimiento a la Izquierda.
         if (Input.GetKey(KeyCode.A))
         {
-            transform.Translate(-velocidad * Time.deltaTime, 0, 0, Space.Self);
+            transform.Translate(-velocidadJugador * Time.deltaTime, 0, 0, Space.Self);
         }
         //Movimiento a la Derecha.
         if (Input.GetKey(KeyCode.D))
         {
-            transform.Translate(velocidad * Time.deltaTime, 0, 0, Space.Self);
+            transform.Translate(velocidadJugador * Time.deltaTime, 0, 0, Space.Self);
         }
         //Movimiento a hacia Arriba.
         if (Input.GetKey(KeyCode.S))
         {
-            transform.Translate(0, -velocidad * Time.deltaTime, 0, Space.Self);
+            transform.Translate(0, -velocidadJugador * Time.deltaTime, 0, Space.Self);
         }
     }
+
+    public void CheckLife()
+    {
+        if (vidaJugador <=0)
+        {
+            Destroy(gameObject);
+            SceneManager.LoadScene(3); //Abre escena 3 para voler a jugar o salir del juego.
+        }
+    }
+
+    //Metodo para que el jugador pueda disparar.
+    public void DisparoJugador()
+    {
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+
+            Instantiate(bala, posicionesBala[0].position, Quaternion.identity);
+            Instantiate(bala, posicionesBala[1].position, Quaternion.identity);
+        }
+
+    }
+    public void BajarvidaJugador()
+    {
+        if (Input.GetKey(KeyCode.O))
+        {
+            vidaJugador--;
+        }
+
+    }
+
+
 }
